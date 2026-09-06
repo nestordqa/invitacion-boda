23271c3147111bab5116425ad3ea9499da69c8e2
Nestor Quinones <nquinones@messangi.com>
2026-09-06 15:53:30 -0400

    fix: fixing button bug

--- components/v2/Intro.tsx
+++ components/v2/Intro.tsx
@@ -40,7 +40,7 @@ export function Intro({ guest, showCta, onNavigateNext }: IntroProps) {
           className="absolute left-1/2 top-[42%] z-20 size-24 -translate-x-1/2 rounded-full focus:outline-none focus:ring-2 focus:ring-[#f7e7a6]/80 sm:size-28"
         />
       )}
-      <div className="relative inset-x-0 top-72 z-10 flex justify-center">
+      <div className="relative inset-x-0 top-64 z-10 flex justify-center">
         <AnimatePresence>
           {showCta && (
             <motion.div
